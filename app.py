79a782ca6803ed2f3d1351938813cb6b6bf7d186
gomex500 <gomezfreddy886@gmail.com>
2026-02-10 00:40:43 -0600

    otro guardao

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config.config import DEBUG, PORT
 from flask_cors import CORS
 from routers.login_router import login_routes
+from routers.user_router import user_routes
 from routers.home import home
 
 app = Flask(__name__)
@@ -13,6 +14,7 @@ CORS(app)
 #routes
 app.register_blueprint(home)
 app.register_blueprint(login_routes)
+app.register_blueprint(user_routes)
 
 
 if __name__ == "__main__":
